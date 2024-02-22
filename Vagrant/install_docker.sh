b14d32d003004b7edf7bef4a78d47686059718c0
willmiiller <will.miiller@hotmail.com>
2024-02-21 22:39:38 -0300

    comit vagrant

--- Vagrant/install_docker.sh
+++ Vagrant/install_docker.sh
@@ -1,13 +1,8 @@
 #!/bin/bash
 
-sudo ufw disable
-sudo systemctl stop firewalld
-sudo systemctl disable firewalld
-
-
 # Add Docker's official GPG key:
-sudo apt-get update
-sudo apt-get install ca-certificates curl
+sudo apt-get update -y
+sudo apt-get install ca-certificates curl -y
 sudo install -m 0755 -d /etc/apt/keyrings
 sudo curl -fsSL https://download.docker.com/linux/ubuntu/gpg -o /etc/apt/keyrings/docker.asc
 sudo chmod a+r /etc/apt/keyrings/docker.asc
@@ -17,6 +12,6 @@ echo \
   "deb [arch=$(dpkg --print-architecture) signed-by=/etc/apt/keyrings/docker.asc] https://download.docker.com/linux/ubuntu \
   $(. /etc/os-release && echo "$VERSION_CODENAME") stable" | \
   sudo tee /etc/apt/sources.list.d/docker.list > /dev/null
-sudo apt-get update
+sudo apt-get update -y
 
-sudo apt-get install docker-ce docker-ce-cli containerd.io
+sudo apt-get install docker-ce docker-ce-cli containerd.io -y
